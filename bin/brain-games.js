543ced6213025313cf50198de6d394f028fc0608
Vlad Filiucov <filiucov@mail.ru>
2020-04-20 21:37:24 +0100

    start adding brain even game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import promptAndPrintPlayersName from '../src/index.js';
+import { promptAndPrintPlayersName } from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
 promptAndPrintPlayersName();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+console.log('Welcome to the Brain Games!');
+
+export const promptAndPrintPlayersName = () => {
   const playerName = readlineSync.question('May I have your name? - ');
   console.log(`Hello, ${playerName}!`);
+
+  return playerName;
+};
+
+const VALID_ANSWERS = ['yes', 'no'];
+
+const askBrainEvenQuestions = (playerName) => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Question: 15');
+  const answer = readlineSync.question('Your answer: ');
+
+  if (!VALID_ANSWERS.includes(answer.toLowerCase())) {
+    console.log('is wrong answer ;(. Correct answer was "no".');
+    console.log(`Let's try again, ${playerName}!`);
+  }
+};
+
+export const playBrainEven = () => {
+  const playerName = promptAndPrintPlayersName();
+
+  while (true) {
+    askBrainEvenQuestions(playerName);
+  }
 };
