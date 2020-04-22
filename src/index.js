04c05628fc2651db9d17b5b90771d5951f2d25c9
Vlad Filiucov <filiucov@mail.ru>
2020-04-22 22:45:49 +0100

    add brain-even logic

--- src/index.js
+++ src/index.js
@@ -9,23 +9,43 @@ export const promptAndPrintPlayersName = () => {
   return playerName;
 };
 
-const VALID_ANSWERS = ['yes', 'no'];
+const generateRandomNumber = (max) => Math.floor(Math.random() * Math.floor(max));
+const logCorrectAnswerOnMistake = (correctAnswer) => console.log(`is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+const logTryAgainMessage = (playerName) => console.log(`Let's try again, ${playerName}!`);
 
 const askBrainEvenQuestions = (playerName) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log('Question: 15');
-  const answer = readlineSync.question('Your answer: ');
+  let numberOfCorrectAnswersGiven = 0;
+  let commitedMistake = false;
 
-  if (!VALID_ANSWERS.includes(answer.toLowerCase())) {
-    console.log('is wrong answer ;(. Correct answer was "no".');
-    console.log(`Let's try again, ${playerName}!`);
+  const playRound = () => {
+    const currentNumber = generateRandomNumber(100);
+    const correctAnswerIsEven = currentNumber % 2 === 0;
+    const correctAnswer = correctAnswerIsEven ? 'yes' : 'no';
+    console.log(`Question: ${currentNumber}`);
+    const playersAnswer = readlineSync.question('Your answer: ');
+
+    if (playersAnswer && playersAnswer.toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+      numberOfCorrectAnswersGiven += 1;
+      return;
+    }
+
+    logCorrectAnswerOnMistake(correctAnswer);
+    logTryAgainMessage(playerName);
+    commitedMistake = true;
+  };
+
+
+  while (!commitedMistake && numberOfCorrectAnswersGiven < 3) {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    playRound();
   }
+
+  if (!commitedMistake) console.log(`Congratulations, ${playerName}!`);
 };
 
 export const playBrainEven = () => {
   const playerName = promptAndPrintPlayersName();
 
-  while (true) {
-    askBrainEvenQuestions(playerName);
-  }
+  askBrainEvenQuestions(playerName);
 };
