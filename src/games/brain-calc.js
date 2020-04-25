ab4c9313bed2eec7411d352752107cb5a243773a
Vlad Filiucov <filiucov@mail.ru>
2020-04-25 17:27:53 +0100

    add brain-calc game base logic

---
+++ src/games/brain-calc.js
@@ -0,0 +1,61 @@
+import {
+  printWelcomeMessage,
+  getNameFromPlayer,
+  getPlayersAnswer,
+  logCorrectAnswerOnMistake,
+  logTryAgainMessage,
+  logCorrectAnswerMessage,
+  greetPlayer,
+  logQuestion,
+} from '../messageHelpers.js';
+import generateRandomNumber from '../utils/index.js';
+
+const sum = (a, b) => a + b;
+const neg = (a, b) => a - b;
+const mult = (a, b) => a * b;
+const supportedOperations = [
+  ['+', sum],
+  ['-', neg],
+  ['*', mult],
+];
+
+const playBrainCalc = playersName => {
+  let numberOfCorrectAnswersGiven = 0;
+  let commitedMistake = false;
+
+  const playRound = () => {
+    const firstNumber = generateRandomNumber(10);
+    const secondNumber = generateRandomNumber(10);
+    const currentOperation = supportedOperations[generateRandomNumber(supportedOperations.length)];
+    const [operator, cb] = currentOperation;
+    logQuestion(`${firstNumber} ${operator} ${secondNumber}`);
+    const playersAnswer = getPlayersAnswer();
+    const parsedPlayersAnswer = Number(playersAnswer);
+    const correctAnswer = cb(firstNumber, secondNumber);
+
+    if (!Number.isNaN(parsedPlayersAnswer) && parsedPlayersAnswer === correctAnswer) {
+      logCorrectAnswerMessage();
+      numberOfCorrectAnswersGiven += 1;
+      return;
+    }
+
+    logCorrectAnswerOnMistake(correctAnswer);
+    logTryAgainMessage(playersName);
+    commitedMistake = true;
+  };
+
+  while (!commitedMistake && numberOfCorrectAnswersGiven < 3) {
+    console.log('What is the result of the expression?');
+    playRound();
+  }
+
+  if (!commitedMistake) console.log(`Congratulations, ${playersName}!`);
+};
+
+export default () => {
+  printWelcomeMessage();
+  const playersName = getNameFromPlayer();
+  greetPlayer(playersName);
+
+  playBrainCalc(playersName);
+};
