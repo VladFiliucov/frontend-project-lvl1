2bb464fe908dc98b2e16353a07952214b876995a
Vlad Filiucov <filiucov@mail.ru>
2020-04-28 22:29:02 +0100

    add new game logic

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,68 @@
+import {
+  printWelcomeMessage,
+  getNameFromPlayer,
+  getPlayersAnswer,
+  logCorrectAnswerOnMistake,
+  logTryAgainMessage,
+  logCorrectAnswerMessage,
+  greetPlayer,
+  logQuestion,
+  logMessage,
+  logVictoryMessage,
+} from '../messageHelpers.js';
+import generateRandomNumber from '../utils/index.js';
+
+const playBrainGcd = playersName => {
+  let numberOfCorrectAnswersGiven = 0;
+  let commitedMistake = false;
+
+  const playRound = () => {
+    const firstNumber = generateRandomNumber(30);
+    const secondNumber = generateRandomNumber(30);
+
+    const findGreatestDivisor = (a, b) => {
+      if (a === b) return a;
+      if (a === 0 || b === 0) return 0;
+
+      const lowerNumber = a < b ? a : b;
+
+      for (let i = lowerNumber; i >= 1; i -= 1) {
+        if (a % i === 0 && b % i === 0) {
+          return i;
+        }
+      }
+      return 1;
+    };
+
+    const correctAnswer = findGreatestDivisor(firstNumber, secondNumber);
+    logQuestion(`${firstNumber} ${secondNumber}`);
+
+    const playersAnswer = getPlayersAnswer();
+    const parsedPlayersAnswer = Number(playersAnswer);
+
+    if (!Number.isNaN(parsedPlayersAnswer) && parsedPlayersAnswer === correctAnswer) {
+      logCorrectAnswerMessage();
+      numberOfCorrectAnswersGiven += 1;
+      return;
+    }
+
+    logCorrectAnswerOnMistake(correctAnswer);
+    logTryAgainMessage(playersName);
+    commitedMistake = true;
+  };
+
+  while (!commitedMistake && numberOfCorrectAnswersGiven < 3) {
+    logMessage('Find the greatest common divisor of given numbers.');
+    playRound();
+  }
+
+  if (!commitedMistake) logVictoryMessage(playersName);
+};
+
+export default () => {
+  printWelcomeMessage();
+  const playersName = getNameFromPlayer();
+  greetPlayer(playersName);
+
+  playBrainGcd(playersName);
+};
