342367b59110d200c08a0bfe39bb6863e05cbce1
Vlad Filiucov <filiucov@mail.ru>
2020-05-05 20:43:07 +0100

    remove redundant game

--- bin/brain-games.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import playBrainGames from '../src/games/brain-games.js';
-
-playBrainGames();
--- src/games/brain-games.js
+++
@@ -1,7 +0,0 @@
-import { printWelcomeMessage, getNameFromPlayer, greetPlayer } from '../messageHelpers.js';
-
-export default () => {
-  printWelcomeMessage();
-  const playersName = getNameFromPlayer();
-  greetPlayer(playersName);
-};
