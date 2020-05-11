e7413a61205e03581c9a5f9d5eebf17d5e356ac2
Vlad Filiucov <filiucov@mail.ru>
2020-05-11 15:35:35 +0100

    refactor game core

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import readlineSync from 'readline-sync';
 const ATTEMPTS_TO_WIN = 3;
 
 export default (rules, genGameData) => {
-  let numberOfCorrectAnswersGiven = 0;
-  let commitedMistake = false;
-
   console.log('Welcome to the Brain Games!');
 
   const playersName = readlineSync.question('May I have your name? - ') || 'Player1';
   console.log(`Hello, ${playersName}!`);
 
-  const playRound = () => {
+  console.log(rules);
+  for (let i = 1; i <= ATTEMPTS_TO_WIN; i += 1) {
     const [question, correctAnswer] = genGameData();
 
     console.log(`Question: ${question}`);
@@ -19,19 +17,13 @@ export default (rules, genGameData) => {
 
     if (playersAnswer === correctAnswer) {
       console.log('Correct!');
-      numberOfCorrectAnswersGiven += 1;
-      return;
+      if (i === ATTEMPTS_TO_WIN) {
+        console.log(`Congratulations, ${playersName}!`);
+      }
+    } else {
+      console.log(`${playersAnswer} is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${playersName}!`);
+      break;
     }
-
-    console.log(`${playersAnswer} is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-    console.log(`Let's try again, ${playersName}!`);
-    commitedMistake = true;
-  };
-
-  while (!commitedMistake && numberOfCorrectAnswersGiven < ATTEMPTS_TO_WIN) {
-    console.log(rules);
-    playRound();
   }
-
-  if (!commitedMistake) console.log(`Congratulations, ${playersName}!`);
 };
