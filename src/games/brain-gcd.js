3a22a2aa2209e5b49647a6b4d78d3ba0f33bd07e
Vlad Filiucov <filiucov@mail.ru>
2020-05-11 14:34:58 +0100

    refactor gcd game

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,35 +1,33 @@
 import gameCore from '../index.js';
 import generateRandomNumber from '../utils/index.js';
 
-const playGcdRound = () => {
+const findGreatestDivisor = (a, b) => {
+  if (a === b) return a;
+  if (a === 0 || b === 0) return 0;
+
+  const lowerNumber = a < b ? a : b;
+
+  for (let i = lowerNumber; i >= 1; i -= 1) {
+    if (a % i === 0 && b % i === 0) {
+      return i;
+    }
+  }
+  return 1;
+};
+
+const genGameData = () => {
   const firstNumber = generateRandomNumber(0, 30);
   const secondNumber = generateRandomNumber(0, 30);
 
-  const findGreatestDivisor = (a, b) => {
-    if (a === b) return a;
-    if (a === 0 || b === 0) return 0;
+  const correctAnswer = findGreatestDivisor(firstNumber, secondNumber).toString();
 
-    const lowerNumber = a < b ? a : b;
+  const question = `${firstNumber} ${secondNumber}`;
 
-    for (let i = lowerNumber; i >= 1; i -= 1) {
-      if (a % i === 0 && b % i === 0) {
-        return i;
-      }
-    }
-    return 1;
-  };
-
-  const correctAnswer = findGreatestDivisor(firstNumber, secondNumber);
-
-  const questionedNumbers = `${firstNumber} ${secondNumber}`;
-
-  return [questionedNumbers, correctAnswer];
+  return [question, correctAnswer];
 };
 
-const gameIntroMessage = 'Find the greatest common divisor of given numbers.';
-const validateAnswer = answer => !Number.isNaN(Number(answer));
-const normalizeAnswer = answer => Number(answer);
+const rules = 'Find the greatest common divisor of given numbers.';
 
 export default () => {
-  gameCore(gameIntroMessage, playGcdRound, validateAnswer, normalizeAnswer);
+  gameCore(rules, genGameData);
 };
